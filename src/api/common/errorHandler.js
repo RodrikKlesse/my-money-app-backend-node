9193d67ce242c0c5bb851a1dd99a61dd9ac7c4dc
RodrikKlesse <rodrik_hsk@hotmail.com>
2020-01-17 11:30:25 -0300

    Correção no tratamento de erro

--- src/api/common/errorHandler.js
+++ src/api/common/errorHandler.js
@@ -3,7 +3,7 @@ const _ = require('lodash')
 module.exports = (req, res, next) => {
     const bundle = res.locals.bundle
 
-    if (bundle.erros) {
+    if (bundle.errors) {
         const errors = parseErrors(bundle.errors)
         res.status(500).json({ errors })
     } else {
